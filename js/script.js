3a57d516bbc94eb6500d0c52bbe8d8b8a95ce746
SmzFz <felipesalmazo123@gmail.com>
2023-08-10 10:21:31 -0300

    Finalização Foto de Perfil

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 var ToggleMode = () => {
 const html = document.documentElement;
-
 html.classList.toggle("light");
+const isLight = html.classList.contains("light");
+const srcValue = isLight ? "../img/avatar-light.jpg" : "../img/avatar.png";
+const altValue = isLight ? "Imagem de perfil com um avatar na cor azul" : "Imagem de perfil na cor vermelha";
+document.querySelector("#profile img").setAttribute("src",srcValue);
+document.querySelector("#profile img").setAttribute("alt", altValue);
 
 };
 
